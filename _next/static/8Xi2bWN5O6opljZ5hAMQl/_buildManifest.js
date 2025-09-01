7bc64724216cc97c766f9c4a07a35185c000e05f
Jameel Shaikh <42856910+JameelShaikh32@users.noreply.github.com>
2025-09-01 19:41:24 +0000

    Updates

---
+++ _next/static/8Xi2bWN5O6opljZ5hAMQl/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/6417816e69e4830f.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/6417816e69e4830f.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/03180ede221f8ac0.js",
+  "static/chunks/turbopack-9aa26baf3c7eab04.js"
+])
